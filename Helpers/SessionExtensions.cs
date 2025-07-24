d9e0840bc8dde6da56afc20820480aa34c50cbfa
BonganiChauke <bonganichauke11@outlook.com>
2025-07-24 23:05:00 +0200

    Session class for cart functionality

---
+++ Helpers/SessionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace CafeteriaSystem.Helpers
+{
+    public static class SessionExtensions
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        public static T GetObjectFromJson<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+#pragma warning disable CS8603 // Possible null reference return.
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+    }
+}
