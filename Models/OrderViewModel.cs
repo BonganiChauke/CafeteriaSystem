7061c895275ffa19a412aeb20176fc415be24082
BonganiChauke <bonganichauke11@outlook.com>
2025-07-26 20:53:11 +0200

    Added restaurant name property

--- Models/OrderViewModel.cs
+++ Models/OrderViewModel.cs
@@ -12,7 +12,7 @@ namespace CafeteriaSystem.Models
         public List<MenuItem> MenuItems { get; set; } = new List<MenuItem>();
         public int MenuItemId { get; set; }
         public string? MenuItemName { get; set; }
-        public decimal? UnitPrice { get; set; } 
+        public decimal UnitPrice { get; set; } 
         public int Quantity { get; set; }
         public SelectList? RestaurantOptions { get; set; }
         public List<OrderItemViewModel>? CartItems { get; set; }
