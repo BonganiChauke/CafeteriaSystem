c9139df0e73006e52749f6c1128bc35578a2d8eb
BonganiChauke <bonganichauke11@outlook.com>
2025-07-26 20:52:32 +0200

    Added restaurant property name

--- Models/OrderItemViewModel.cs
+++ Models/OrderItemViewModel.cs
@@ -5,8 +5,9 @@
         public int MenuItemId { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }  
-        public decimal UnitPrice { get; internal set; }
-        public string? MenuItemName { get; internal set; }
-        
+        public decimal UnitPrice { get; set; }
+        public string? MenuItemName { get; set; }
+        public string? RestaurantName { get; set; }
+
     }
 }
